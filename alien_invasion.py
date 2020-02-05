d18c2521bda7c1b22de5067632ca731cf9dea26c
Pavel Nikiforov <velfor@tut.by>
2020-02-05 09:42:47 +0300

    Moved game settings to settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,16 +5,16 @@ from pygame.locals import *
 def run_game():
 # Инициализирует игру и создает объект экрана
     pygame.init()
-    screen = pygame.display.set_mode((1200, 700))
+    gameSettings = Settings()
+    screen = pygame.display.set_mode((gameSettings.screenWidth, gameSettings.screenHeight))
     pygame.display.set_caption("Alien Invasion")
-    bgColor = (230,230,230)
     while True:
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
     
-        screen.fill(bgColor)
+        screen.fill(gameSettings.bgColor)
         pygame.display.flip()
 #end run_game
 run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+#Класс для хранения всех настроек игры Alien Invasion.
+    def __init__(self):
+        #Инициализирует настройки игры.
+        # Параметры экрана
+        self.screenWidth = 1200
+        self.screenHeight = 700
+        self.bgColor = (230, 230, 230)
